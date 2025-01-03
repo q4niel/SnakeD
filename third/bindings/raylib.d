4f8229e39a2e933caf894805137b56da7e4b3943
q4niel <github.obscure938@passmail.net>
2025-01-03 13:10:26 +0100

    Removed comment from 'raylib.d' binding.

--- third/bindings/raylib.d
+++ third/bindings/raylib.d
@@ -2,7 +2,7 @@ extern(C) {
     void InitWindow(int width, int height, const char *title);
     void CloseWindow();
     bool WindowShouldClose();
-    void BeginDrawing();                                    // Setup canvas (framebuffer) to start drawing
+    void BeginDrawing();
     void EndDrawing();
     void SetTargetFPS(int fps);
 }
