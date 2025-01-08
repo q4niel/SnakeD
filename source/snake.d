75c7ca7d44c2cc7fabb159f22cefd77dabc93e29
q4niel <github.obscure938@passmail.net>
2025-01-08 15:59:51 +0100

    readme + preview image. Snake self and wall collision respawn, and wait for first input until movement.

--- source/snake.d
+++ source/snake.d
@@ -4,25 +4,41 @@ import third.glue.raylib.raymath;
 import data;
 import engine;
 import food;
+import std.stdio;
 
 class Snake : IEntity {
+    bool isMoving = false;
+
     Vector2[] body = [
-        {6, 9},
-        {5, 9},
-        {4, 9}
+        Vector2(),
+        Vector2(),
+        Vector2()
     ];
 
-    Vector2 dir = {1, 0};
+    Vector2 dir = Vector2();
 
-    void onInitialize() {}
+    void onInitialize() {
+        respawn();
+    }
 
     void onProcess() {
-        for (int i = 0; i < body.length; i++) {
-            if (IsKeyPressed(KEY_UP)) dir = Vector2(0, -1);
-            if (IsKeyPressed(KEY_DOWN)) dir = Vector2(0, 1);
-            if (IsKeyPressed(KEY_LEFT)) dir = Vector2(-1, 0);
-            if (IsKeyPressed(KEY_RIGHT)) dir = Vector2(1, 0);
+        if (!isMoving) {
+            if (
+                IsKeyPressed(KEY_UP)
+            ||  IsKeyPressed(KEY_DOWN)
+            ||  IsKeyPressed(KEY_LEFT)
+            ||  IsKeyPressed(KEY_RIGHT)
+            ) {
+                isMoving = true;
+            }
+        }
 
+        if (IsKeyPressed(KEY_UP) && dir.y != 1) dir = Vector2(0, -1);
+        if (IsKeyPressed(KEY_DOWN) && dir.y != -1) dir = Vector2(0, 1);
+        if (IsKeyPressed(KEY_LEFT) && dir.x != 1) dir = Vector2(-1, 0);
+        if (IsKeyPressed(KEY_RIGHT) && dir.x != -1) dir = Vector2(1, 0);
+
+        for (int i = 0; i < body.length; i++) {
             DrawRectangleRounded (
                 Rectangle (
                     cast(int) body[i].x * Data.cellSize,
@@ -42,15 +58,46 @@ class Snake : IEntity {
             body = Vector2Add(body[0], dir) ~ body;
         }
 
-        grow();
-        body = body[0..$-1];
+        if (isMoving) {
+            grow();
+            body = body[0..$-1];
+        }
 
         Food food = Engine.getFood();
         if (Vector2Equals(body[0], food.pos)) {
             food.respawn();
             grow();
         }
+
+        if (
+            body[0].x >= Data.cellCount
+        ||  body[0].x <= -1
+        ||  body[0].y >= Data.cellCount
+        ||  body[0].y <= -1
+        ) {
+            respawn();
+        }
+
+        foreach (part; body[1..$]) {
+            if (Vector2Equals(body[0], part)) {
+                respawn();
+            }
+        }
     }
 
     void onTerminate() {}
+
+    void respawn() {
+        isMoving = false;
+
+        body = [
+            Vector2(6, 9),
+            Vector2(5, 9),
+            Vector2(4, 9)
+        ];
+
+        dir = Vector2(1, 0);
+
+        Engine.getFood().respawn();
+    }
 }
